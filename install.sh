fa8aa006e1628b47a2b34b0b800f71bd3beece2d
ottjk <joshott16@gmail.com>
2023-12-31 14:24:59 -0500

    pacman packages

---
+++ install.sh
@@ -0,0 +1,47 @@
+sudo pacman -Syu \
+    blueman \
+    brightnessctl \
+    dex \
+    dunst \
+    eza \
+    firefox \
+    fish \
+    fzf \
+    grim \
+    gnome-keyring \
+    imv \
+    ly \
+    neovim \
+    papirus-icon-theme \
+    pavucontrol \
+    pcmanfm-gtk3 \
+    ripgrep \
+    ripgrep \
+    rust \
+    sshfs \
+    starship \
+    texlive-basic \
+    texlive-bibtexextra \
+    texlive-binextra \
+    texlive-fontsextra \
+    texlive-formatsextra \
+    texlive-latexextra \
+    texlive-mathscience \
+    texlive-pictures \
+    unzip \
+    vlc \
+    wayland-protocols \
+    wezterm \
+    wget \
+    wireguard-tools \
+    xplr \
+    zoxide \
+    gvim \
+    keepassxc \
+    nextcloud-client \
+    noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
+    otf-font-awesome \
+    sway swaybg swayidle \
+    tmux \
+    waybar \
+    zathura-pdf-mupdf
