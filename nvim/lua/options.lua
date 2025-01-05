37aaa8ed78406ec3ace4fabab5b725fb11321526
Josh <joshott16@gmail.com>
2025-01-05 15:15:03 -0500

    added feishin to sway and fixed neovim indenting

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,7 +6,8 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
+vim.opt.smartindent = false
+vim.opt.autoindent = true
 
 vim.opt.wrap = false
 
