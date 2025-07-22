af2750e922c20c2cd2c24111adfac762264eaf15
Josh <joshott16@gmail.com>
2025-07-22 13:16:24 -0400

    removing old nvim plugin configs

--- nvim/after/plugin/bufdel.lua
+++
@@ -1,3 +0,0 @@
-require('bufdel').setup {
-  quit = false,    -- quit Neovim when last buffer is closed
-}
--- nvim/after/plugin/nvim-markdown.lua
+++
@@ -1,9 +0,0 @@
-vim.g.vim_markdown_conceal = 2
--- vim.g.vim_markdown_math = 1
-vim.g.vim_markdown_toc_autofit = 1
-
-local map = vim.keymap.set;
-local opts = { noremap = false, silent = true}
-
-map('', 'o', '<Plug>Markdown_NewLineBelow<Esc>', opts)
-map('', 'O', '<Plug>Markdown_NewLineAbove<Esc>', opts)
